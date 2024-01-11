f82e3b326c0872baa383a347fb9c0e5e70672504
Oscar Soler <oscarsoler@MacBook-Air-de-Oscar.local>
2024-01-10 20:02:37 -0500

    Config git signs nvim

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -10,5 +10,26 @@ return {
     vim.keymap.set('n', '<leader>t', '<Cmd>Neotree toggle<CR>')
     vim.keymap.set("n", "<leader>f", ":Neotree filesystem reveal left<CR>", {})
     vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+    require('nvim-web-devicons').setup()
+
+    require('neo-tree').setup({
+      enable_git_status = true,
+      default_component_configs = {
+        git_status = {
+          symbols = {
+            added     = "",  -- Ícono de verificación (representa un archivo agregado)
+            deleted   = "",  -- Ícono de cruz (representa un archivo eliminado)
+            modified  = "✎",  -- Ícono de lápiz (representa un archivo modificado)
+            renamed   = "凜",  -- Ícono de flechas (representa un archivo renombrado)
+            untracked = "",  -- Ícono de interrogación (representa un archivo no rastreado)
+            ignored   = "",  -- Ícono de ojo cerrado (representa un archivo ignorado)
+            unstaged  = "",  -- Ícono de círculo (representa cambios no preparados)
+            staged    = "",  -- Ícono de marcador (representa cambios preparados)
+            conflict  = "",  -- Ícono de alerta (representa conflictos)
+          }
+        }
+      }
+    })
   end
 }
+
